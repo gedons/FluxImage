b06fc7c9d0dad9fa80aead26bc4b1d46a4111795
gedons <gedoniani@gmail.com>
2023-10-24 16:37:02 +0100

    Untrack project JSON file in server folder

--- server/config/config.js
+++
@@ -1,7 +0,0 @@
-const env = require('dotenv').config();
-
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  googleAppCredentials: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-};
